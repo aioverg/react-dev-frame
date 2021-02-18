0e88721c4af6a2f7991dd553a67a43b297407bd4
aioverg <aioverg@outlook.com>
2021-02-18 17:21:54 +0800

    011

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,8 @@
-import createSagaMiddleware from 'redux-saga'
-import { createStore, applyMiddleware } from 'redux'
-import reducer from './rootReducer'
-import sagas from './rootSaga'
-// import rootReducer from './reducers'
+import { createStore } from 'redux'
+import reducers from './reducers'
 
-const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware)
+  reducers
 )
-sagaMiddleware.run(sagas)
 
 export default store
