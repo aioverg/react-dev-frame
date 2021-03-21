527451afcb15c03cda63a7f4ead8a8b4e718fbcf
aioverg <aioverg@outlook.com>
2021-03-21 13:59:05 +0800

    codemirror

--- src/views/CodeMirror/index.js
+++ src/views/CodeMirror/index.js
@@ -1,11 +1,11 @@
 import React, { PureComponent, useState  } from 'react'
 import treeData from '@src/comm/js/tree'
-import CodeMirror from 'codemirror'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/mode/sql/sql'
-import 'codemirror/addon/hint/show-hint.css'
-import 'codemirror/addon/hint/sql-hint.js'
-import 'codemirror/addon/hint/show-hint.js'
+import CodeMirror from 'codemirror' // 引入 codemirror 包
+import 'codemirror/lib/codemirror.css' // 引入 codemirror 样式
+import 'codemirror/mode/sql/sql' // 引入 sql 语言
+import 'codemirror/addon/hint/show-hint.css' // 引入输入提示样式
+import 'codemirror/addon/hint/sql-hint.js' // 引入 sql 输入提示
+import 'codemirror/addon/hint/show-hint.js' // 引入输入提示
 
 
 class Catalog extends PureComponent {
@@ -27,21 +27,21 @@ class Catalog extends PureComponent {
       </div>
     )
   }
-  onClick = () => {
+  onClick = () => { // 打印输入的代码
     const {codeMirrorObj} = this.state
     const code = codeMirrorObj.getValue()
-    console.log(99999, code)
+    console.log(code)
     
   }
-  codeMirror = (ref) => {
+  codeMirror = (ref) => { // 挂载 codemirror 编辑器
     const codeMirrorObj = CodeMirror(ref, {
-      value: "select * from test where id='${abc}';",
-      mode:  "sql",
-      extraKeys: {"Ctrl": "autocomplete"}
+      value: "select * from test where id='${abc}';", // 初始化输入的内容
+      mode:  "sql", // 使用 sql 语言
+      extraKeys: {"Ctrl": "autocomplete"} // 设置按 Ctrl 键显示输入提示
     });
     if(ref){
-      ref.addEventListener('cursorActivity', () => {
-        ref.showHint()
+      ref.addEventListener('cursorActivity', () => { // 监听 cursorActivity 事件
+        ref.showHint() // 挂在输入提示
       })
     }
     
@@ -52,7 +52,7 @@ class Catalog extends PureComponent {
   }
   componentWillUnmount() {
     const {codeMirrorRef} = this.state
-    codeMirrorRef.removeEventListener('cursorActivity', () => {
+    codeMirrorRef.removeEventListener('cursorActivity', () => { // 销毁监听事件
       codeMirrorRef.showHint()
     })
   }
@@ -60,12 +60,20 @@ class Catalog extends PureComponent {
 
   render() {
     return (
-      <>
-        <h2 style={{ textAlign: 'center', fontSize: '20px' }}>首页111111</h2>
-        <button onClick={this.onClick}>点击</button>
-        <div id="container" style={{width: '500px', height: '500px'}} ref={this.codeMirror}>
+      <div style={{padding: '0 20px'}}>
+        <h2 style={{ textAlign: 'center', fontSize: '20px' }}>codemirror 代码编辑器插件</h2>
+        <div>
+          <p>github 地址：<a href='https://github.com/codemirror/CodeMirror'>https://github.com/codemirror/CodeMirror</a></p>
+          <p>官方地址：<a href='https://codemirror.net/6/'>https://codemirror.net/6/</a></p>
+          <p>基本使用讲解地址：<a href="http://www.tronbird.com/">暂略</a></p>
         </div>
-      </>
+        <div>
+          <p>示例：以 sql 语言输入为例，按 Ctrl 键显示输入提示的编辑器</p>
+          <button onClick={this.onClick}>打印输入的代码</button>
+          <div id="container" style={{width: '500px', height: '350px', border: '1px solid #D8D8D8', marginTop: '18px'}} ref={this.codeMirror}></div>
+        </div>
+        
+      </div>
     )
   }
 }
